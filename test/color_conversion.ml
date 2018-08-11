b7b351642ababd6aecae85d8196e1dd796282f5a
Anurag Soni <anuragsoni.13@gmail.com>
2018-08-11 18:15:31 -0400

    Add more tests

--- test/color_conversion.ml
+++ test/color_conversion.ml
@@ -20,4 +20,37 @@ let test_hsl_to_rgba () =
     "can convert hsl to rgba" true
     (List.for_all (fun x -> x) compare)
 
-let tests = [("Hsl to RGBA'", `Quick, test_hsl_to_rgba)]
+let colors_rgba_hex =
+  [ (0, 191, 255, "#00bfff")
+  ; (98, 121, 255, "#6279ff")
+  ; (198, 167, 55, "#c6a737")
+  ; (198, 17, 55, "#c61137")
+  ; (252, 7, 16, "#fc0710")
+  ; (18, 157, 126, "#129d7e")
+  ; (98, 167, 255, "#62a7ff") ]
+
+let test_color_to_hexstring () =
+  let check (r, g, b, h) =
+    let c = Color.of_rgb r g b in
+    Alcotest.(check string)
+      "Converts color to hexstring" h (Color.to_hexstring c)
+  in
+  List.iter (fun x -> check x) colors_rgba_hex
+
+let test_hexstring_to_color () =
+  let open Color in
+  let check (r, g, b, h) =
+    match of_hexstring h with
+    | Some c ->
+        let c' = to_rgba c in
+        {Rgba.r; g; b; a= 1.} = c'
+    | None -> failwith "Something bad happened"
+  in
+  Alcotest.(check bool)
+    "Can create color from hexstring" true
+    (List.for_all (fun x -> check x) colors_rgba_hex)
+
+let tests =
+  [ ("Hsl to RGBA'", `Quick, test_hsl_to_rgba)
+  ; ("Color to hexstring", `Quick, test_color_to_hexstring)
+  ; ("Hexstring to color", `Quick, test_hexstring_to_color) ]
